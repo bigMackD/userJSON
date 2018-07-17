439fd76aa98fbdb118fdf1cf1346a454fc3063a8
Maciej Drozdowicz <maciek.d@me.com>
2018-07-17 19:51:51 +0200

    Add program files

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tempName, tempSurname, tempDateString;
+            DateTime tempBirthday;
+            Console.WriteLine("Enter name: ");
+            tempName = Console.ReadLine();
+            Console.WriteLine("Enter surname: ");
+            tempSurname = Console.ReadLine();
+            Console.WriteLine("Enter date of birth: ");
+            tempDateString = Console.ReadLine();
+            tempBirthday = DateTime.ParseExact(tempDateString, "yyyy.MM.dd", System.Globalization.CultureInfo.InvariantCulture);
+            User person = new User(tempName, tempSurname, tempBirthday);
+            string jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(person);
+            Console.WriteLine(jsonString);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthday { get; set; }
+        public int Age { get; set; }
+
+        public User(string Name, string Surname, DateTime Birthday)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Birthday = Birthday;
+            Age = DateTime.Now.Year - Birthday.Year;
+
+        }
+    }
+}
